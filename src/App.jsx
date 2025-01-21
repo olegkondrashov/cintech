308ff8f03769c648e1484063ec2d42aee0ce6443
olegkondrashov <condrashov.oleg@gmail.com>
2025-01-21 13:05:25 +0100

    finished main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React, { useState, createContext } from 'react';
+
 import Navigation from "./components/navigation/navigation.component";
 import MainBg from './components/main-bg/main-bg.component';
+import Title from './components/title/title.component';
+
+
 
 export const ThemeContext = createContext();
 
@@ -14,17 +18,21 @@ const App = () => {
 
     const appStyle = {
       backgroundColor: isDark ? '#000' : '#fff',
-      color: isDark ? '#fff' : '#000',
-      minHeight: '100vh',
+      color: isDark ? '#fff' : '#16652A',
+      height: '100vh',
     };
 
     return (
       <ThemeContext.Provider value={{ isDark, toggleTheme }}>
-        <main className='relative md:px-10 lg:px-20' style={appStyle}>
-          <div>
-            <MainBg/>
+        <main className='relative md:px-10 lg:px-20 z-0' style={appStyle}>
+          <MainBg />
+          <Navigation isDark={isDark} className="z-10 text-green-800"/>
+          <Title/>
+          <div className='flex flex-col items-center absolute bottom-5 left-2/4 -translate-x-1/2'>
+            <a href="tel:+495323987427">Tel: 05323/987427</a> 
+            <a href="mailto:info@cintech.de">E-Mail: info@cintech.de</a> 
+            <a href="https://maps.app.goo.gl/KFE247RZnst96SwV6">Gerhard-Rauschenbach-Straße 12 38678 Clausthal-Zellerfeld</a> 
           </div>
-          <Navigation isDark={isDark} />
         </main>
     </ThemeContext.Provider>
     )
---
+++ src/components/title/title.component.jsx
@@ -0,0 +1,26 @@
+import Tilt from "react-parallax-tilt";
+import pc from './pc-bg.png';
+const Title = () => {
+    return (
+        <section className='flex flex-col-reverse	sm:flex-row gap-4 p-4 mt-40 lg:mt-80'>
+            <div className='sm:max-w-screen-md	'>
+              <h1 className='text-4xl md:text-7xl'>Delivering Superior Services IT Solutions.</h1>
+              <p className='mt-5'>treten Sie einfach mit uns in Kontakt.</p>
+              <button className='bg-green-950 rounded-lg px-8 py-3 mt-5'>Kontakt</button>
+            </div>
+            <Tilt
+              className='w-42 sm:w-96'
+              glareEnable={true}
+              glareMaxOpacity={0.5}
+              tiltMaxAngleX={15}
+              tiltMaxAngleY={15}
+              transitionSpeed={250}
+              perspective={500}
+            >
+              <img src={pc} alt="PC" className="tilt-img" />
+            </Tilt>
+          </section>
+    )
+}
+
+export default Title;
