db5116c0b83c92f6a8a1a365d749431c90becc83
olegkondrashov <condrashov.oleg@gmail.com>
2025-01-21 13:45:10 +0100

    changed some styles for mobile version

--- src/components/title/title.component.jsx
+++ src/components/title/title.component.jsx
@@ -3,7 +3,7 @@ import pc from './pc-bg.png';
 const Title = () => {
     return (
         <section className='flex flex-col-reverse sm:flex-row gap-4 p-4 mt-10 lg:mt-80'>
-            <div className='sm:max-w-screen-md	flex flex-col sm:block'>
+            <div className='sm:max-w-screen-md text-center sm:text-left	flex flex-col items-center sm:block'>
               <h1 className='text-4xl md:text-7xl'>Delivering Superior Services IT Solutions.</h1>
               <p className='mt-5'>treten Sie einfach mit uns in Kontakt.</p>
               <button className='bg-green-950 rounded-lg px-8 py-3 mt-5 max-w-xs'>Kontakt</button>
